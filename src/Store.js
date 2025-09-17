6620e0288421f0f0e94ce7de4661d58b808a9c0a
Filippa Johansson <fj222wh@student.lnu.se>
2025-09-17 12:32:48 +0200

    Start implementing class Store

--- src/Store.js
+++ src/Store.js
@@ -2,9 +2,48 @@
  * The store.
  */
 
+import { Order } from "./Order.js"
 import { ProductCatalog } from "./ProductCatalog.js"
 
 export class Store {
+    #name
+    #productCatalog
+    #orders
+    
 
+    constructor(name) {
+        this.setName(name)
+        this.#productCatalog = new ProductCatalog() // OSÄKER? SKA DEN LIGGA HÄR ELLER SKA DEN KUNNA SKAPAS UTANFÖR????
+        this.#orders = []
+    }
+
+    setName(name) {
+        if(name.length <= 0) {
+            throw new Error('The name cannot be empty')
+        }
+
+        this.#name = name
+    }
+
+    displayProducts() {
+        return this.#productCatalog.displayAllProducts()
+    }
+
+    addProductToCatalog(product) {
+        this.#productCatalog.addProduct(product)
+    }
+
+    addOrder(order) {
+        // Controll that we only take instances of Order
+        this.#orders.push(order)
+    }
+
+    cancelOrder() {
+        // TODO: Cancel an order, delete it!!! 
+    }
+
+    viewAllOrders() {
+        return [...this.#orders]
+    }
 }
 
--- src/index.js
+++ src/index.js
@@ -8,18 +8,6 @@ import { Product } from "./Product.js"
 import { ProductCatalog } from "./ProductCatalog.js"
 
 try {
-    const chopchop = new Store('ChopChop')
-    const orangeChicken = new Product('Orange chicken', 50, 'Chicken with orange flavor')
-    chopchop.addProductToCatalog(orangeChicken)
-    const bamboo = new Product('Bamboo', 40, 'hot')
-    chopchop.addProductToCatalog(bamboo)
-    const catalog = chopchop.displayProducts()
-    
-    // SEE EACH OBJECT
-    catalog.forEach((item, index) => {
-        console.log(`Item ${index}:`, item.toString())
-    })
-
 
 
 
